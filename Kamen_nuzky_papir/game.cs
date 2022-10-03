a27fd329edec190eefe3eb3dfcc23057a87291f1
Jurdič Šimon <Jurdic.Simon@stud.vassboskovice.cz>
2022-10-03 14:08:36 +0200

    Přidejte soubory projektu.

---
+++ Kamen_nuzky_papir/game.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System. Timers;
+
+namespace Kamen_nuzky_papir
+{
+    class game
+    {
+        private Timer timer;
+
+        public Player Player1 { get;  }
+
+        public Player Player2 { get;  }
+
+        public game (int money)
+        {
+            Player1 = new Player(money);
+            Player2 = new Player(money);
+            timer = new Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+           
+
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var p1 = Player1.GetStarted();
+            var p2 = Player2.GetStarted();
+            if (p1!=p2)
+            {
+                if (p1 == PlayerStats.Kámen)
+                {
+                    if (p2 == PlayerStats.Nůžky)
+                    {
+                        Player1.Win();
+                        Player2.Lose();
+                    }
+                    else
+                    {
+                        Player1.Lose();
+                        Player2.Win();
+                    }
+                }
+                else if (p1 == PlayerStats.Nůžky)
+                {
+                    if (p2 == PlayerStats.Papír)
+                    {
+                        Player1.Win();
+                        Player2.Lose();
+                    }
+                    else
+                    {
+                        Player1.Lose();
+                        Player2.Win();
+                    }
+                }
+                else if (p1 == PlayerStats.Papír)
+                {
+                    if (p2 == PlayerStats.Kámen)
+                    {
+                        Player1.Win();
+                        Player2.Lose();
+                    }
+                    else
+                    {
+                        Player1.Lose();
+                        Player2.Win();
+                    }
+                }
+            }
+            
+                    
+        }
+    }
+}
---
+++ Kamen_nuzky_papir/player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kamen_nuzky_papir
+{
+    public class Player
+    {
+        Random random = new Random();
+
+        public int Money { get; internal set; }
+
+        public Player(int money)
+        {
+            Money = money;
+
+        }
+
+        public PlayerStats GetStarted()
+        {
+            int value = random.Next(0, 3);
+            return (PlayerStats)value;
+        }
+
+        public void Win()
+        {
+            Money++;
+        }
+
+        public void Lose()
+        {
+            Money--;
+        }
+    }
+
+    public enum PlayerStats
+    {
+        Kámen,
+        Nůžky,
+        Papír,
+    }
+
+}
